bf4d0a531615d6b1709794bf2994f1d998dd0109
sudheer <sudheerreddy9888@gmail.com>
2022-10-26 11:17:19 +0530

    updated application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppointmentsComponent } from './appointments/appointments.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { HomeComponent } from './home/home.component';
+import { PatientsComponent } from './patients/patients.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+path:'',component:HomeComponent
+},
+{
+  path:'home',component:HomeComponent
+  },
+{
+  path:'doctors',component:DoctorsComponent
+},
+{
+  path:'patients',component:PatientsComponent
+},
+{
+  path:'appointments',component:AppointmentsComponent
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { HomeComponent } from './home/home.component';
+import { PatientsComponent } from './patients/patients.component';
+import { AppointmentsComponent } from './appointments/appointments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorsComponent,
+    HomeComponent,
+    PatientsComponent,
+    AppointmentsComponent
   ],
   imports: [
     BrowserModule,
